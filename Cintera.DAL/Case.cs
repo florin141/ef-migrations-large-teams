be886bc87c0680fe24823f7d049b926f683a54be
florin141 <29834996+florin141@users.noreply.github.com>
2019-10-09 23:52:22 +0200

    Revert "Remove unnecesary properties."
    This reverts commit 929a7c0e1d2511fd882ca8979eaccce181bdf79e.

--- Cintera.DAL/Case.cs
+++ Cintera.DAL/Case.cs
@@ -8,5 +8,8 @@ namespace Cintera.DAL
     public class Case
     {
         public int CaseId { get; set; }
+
+        //public virtual ICollection<Sighting> Sightings { get; set; }
+        //public virtual ICollection<Vehicle> Vehicles { get; set; }
     }
 }
