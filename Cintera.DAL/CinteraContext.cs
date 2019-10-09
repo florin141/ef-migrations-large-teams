149b4bf3aeb7427ec818c7b814d2ff0a81249d13
florin141 <29834996+florin141@users.noreply.github.com>
2019-10-09 14:40:38 +0200

    Add and apply migrations.

--- Cintera.DAL/CinteraContext.cs
+++ Cintera.DAL/CinteraContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,14 @@ namespace Cintera.DAL
         {
             
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Case> Cases { get; set; }
     }
 }
